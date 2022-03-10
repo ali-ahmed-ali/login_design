f5d4bf92b5c0144319de0f9d73515de2d2e04e65
ali-ahmed-ali <alilast9898@gmail.com>
2022-03-10 14:30:15 +0530

    created constants.dart class: contains every constant I used in the project

---
+++ lib/utils/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// App Colors
+final Color darkColor = Color(0xFFFFCF54);
+final Color lightColor = Color(0xFFE6E6E6);
+
+class Conststants {
+  BuildContext context;
+  Conststants({required this.context});
+
+  //double _fullWidth() => MediaQuery.of(context).size.width;
+  //double _fullHeight() => MediaQuery.of(context).size.height;
+
+}
